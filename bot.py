f6a43b74886e437cf1643eeba743c71310e808b8
suhaimisuffian12-hash <suhaimisuffian12@gmail.com>
2025-12-10 08:11:44 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
-import os
-os.environ['PORT'] = '8000'
 import telebot
 import requests
 import qrcode
 import io
+import os
 
-BOT_TOKEN = "8381658642:AAHVR0B7KqDFuQl03J7m79Gfekn3Z7S5l1E"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 API_URL = "https://script.google.com/macros/s/AKfycby_DkJrtXgR0cRUti9RXWz0LMeuVHMfxH8Iz8ZDNq8afhaxRzVp3FkUBSZT2wE8k4br/exec"
 
 bot = telebot.TeleBot(BOT_TOKEN)
@@ -17,15 +16,18 @@ def start(msg):
 @bot.message_handler(func=lambda m: True)
 def register(msg):
     user_id = msg.from_user.id
-    name = msg.text
+    name = msg.text.strip()
 
     payload = {
         "user_id": user_id,
         "name": name
     }
 
-    # Send data to Google Apps Script API
-    requests.post(API_URL, json=payload)
+    try:
+        requests.post(API_URL, json=payload)
+    except:
+        bot.reply_to(msg, "❌ Failed to send data to Google Apps Script.")
+        return
 
     # Generate QR
     qr = qrcode.make(f"{user_id}:{name}")
@@ -34,9 +36,9 @@ def register(msg):
     buf.seek(0)
 
     bot.send_photo(
-        msg.chat.id, 
-        buf, 
+        msg.chat.id,
+        buf,
         caption="Here is your QR code."
     )
 
-bot.polling()
+bot.polling(none_stop=True)
